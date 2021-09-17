03f692d2040d02a99a149f1e1c9b9088b071b3ba
Sanchit Oza <sanchithoza@gmail.com>
2021-09-17 18:03:38 +0530

    heroku deploy2

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const fastify = require("./main");
 
-var port_number = server.listen(process.env.PORT || 3000);
+var port_number = fastify.listen(process.env.PORT || 3000);
 app.listen(port_number);
-console.log(`Up on ${PORT}`)
